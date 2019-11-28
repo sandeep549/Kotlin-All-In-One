43f5edaade74edf89eb0c2051fa90a19fb97280f
Sandeep K <sandeepkumar.dev@gmail.com>
2019-11-28 21:05:20 +0530

    Add standard delegate sample

---
+++ app/src/main/java/com/example/kotlinsamples/delegates/StandardDelegates.kt
@@ -0,0 +1,22 @@
+package com.example.kotlinsamples.delegates
+
+import kotlin.properties.Delegates
+import kotlin.reflect.KProperty
+
+fun main() {
+    println(name)
+    println(name)
+
+    println(city)
+    city = "pinkcity"
+}
+
+val name: String by lazy {
+    println("first time")
+    "sam"
+}
+
+var city: String by Delegates.observable("<no name>") {
+        prop, old, new ->
+    println("$old -> $new")
+}
