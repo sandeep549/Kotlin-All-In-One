960294f49eb572a624fad93b1cb02e626f0c2cd1
Sandeep K <sandeepkumar.dev@gmail.com>
2019-12-11 06:44:08 +0530

    344. Reverse String

---
+++ app/src/main/java/com/example/kotlinsamples/leetcode/344. Reverse String.kt
@@ -0,0 +1,21 @@
+package com.example.kotlinsamples.leetcode
+
+fun reverseString(s: CharArray): Unit {
+    for (i in 0..s.size / 2 - 1) {
+        s[i] = s[s.size - 1 - i].also { s[s.size - 1 - i] = s[i] }
+    }
+}
+
+fun reverseString2(s: CharArray): Unit {
+    s.reverse()
+}
+
+fun reverseString3(s: CharArray): Unit {
+    var start = 0
+    var end = s.lastIndex
+    while (start < end) {
+        s[start] = s[end].also { s[end] = s[start] }
+        start++
+        end--
+    }
+}
