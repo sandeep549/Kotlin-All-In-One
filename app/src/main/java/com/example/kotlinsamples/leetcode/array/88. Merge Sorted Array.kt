6ff17380017731b3dba992ae0e10ff645553e666
Sandeep K <sandeepkumar.dev@gmail.com>
2020-01-02 06:18:27 +0530

    88. Merge Sorted Array

---
+++ app/src/main/java/com/example/kotlinsamples/leetcode/array/88. Merge Sorted Array.kt
@@ -0,0 +1,9 @@
+package com.example.kotlinsamples.leetcode.array
+
+private fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
+    var i = m - 1
+    var j = n - 1
+    var k = m + n - 1
+    while (i >= 0 && j >= 0) nums1[k--] = if (nums1[i] >= nums2[j]) nums1[i--] else nums2[j--]
+    while (j >= 0) nums1[k--] = nums2[j--]
+}
