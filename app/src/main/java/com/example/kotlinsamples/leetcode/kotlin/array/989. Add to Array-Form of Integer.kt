5a750297587e6c6e601ac60bf40fb7906f3b9b26
Sandeep K <sandeepkumar.dev@gmail.com>
2020-01-09 03:54:23 +0530

    989. Add to Array-Form of Integer

---
+++ app/src/main/java/com/example/kotlinsamples/leetcode/kotlin/array/989. Add to Array-Form of Integer.kt
@@ -0,0 +1,14 @@
+package com.example.kotlinsamples.leetcode.kotlin.array
+
+private fun addToArrayForm(arr: IntArray, k: Int): List<Int> {
+    var curr = k
+    var ans = ArrayList<Int>()
+    var N = arr.size
+    while (--N >= 0 || curr > 0) {
+        if (N >= 0) curr += arr[N]
+        ans.add(curr % 10)
+        curr = curr / 10
+    }
+    ans.reverse()
+    return ans
+}
