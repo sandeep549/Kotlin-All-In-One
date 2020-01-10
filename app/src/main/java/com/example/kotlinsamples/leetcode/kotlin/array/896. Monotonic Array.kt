5afb3c22db2543f6e969feaccd7e62b839f9075b
Sandeep K <sandeepkumar.dev@gmail.com>
2020-01-10 14:22:03 +0530

    896. Monotonic Array.kt

---
+++ app/src/main/java/com/example/kotlinsamples/leetcode/kotlin/array/896. Monotonic Array.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinsamples.leetcode.kotlin.array
+
+private fun isMonotonic(A: IntArray): Boolean {
+    var inc = true
+    var dec = true
+    for (i in 1 until A.size) {
+        if (A[i] >= A[i - 1]) dec = false
+        if (A[i] <= A[i - 1]) inc = false
+    }
+    return (inc || dec)
+}
