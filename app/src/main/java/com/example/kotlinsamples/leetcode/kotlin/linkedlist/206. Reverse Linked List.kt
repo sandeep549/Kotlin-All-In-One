1801d1c331e198ed24ca1324fdbeae48c3d2dd51
Sandeep K <sandeepkumar.dev@gmail.com>
2020-01-11 18:57:26 +0530

    206. Reverse Linked List.kt

--- app/src/main/java/com/example/kotlinsamples/leetcode/kotlin/linkedlist/206. Reverse Linked List.kt
+++ app/src/main/java/com/example/kotlinsamples/leetcode/kotlin/linkedlist/206. Reverse Linked List.kt
@@ -2,37 +2,25 @@ package com.example.kotlinsamples.leetcode.kotlin.linkedlist
 
 
 // iterative
-private fun reverseList(head: ListNode): ListNode? {
-    var oldhead: ListNode? = head
-    var newhead: ListNode? = null
-    while (oldhead != null) {
-        var tmp = oldhead
-        oldhead = oldhead.next
-        tmp.next = newhead
-        newhead = tmp
+private fun reverseList(head: ListNode?): ListNode? {
+    var hd: ListNode? = head
+    var first: ListNode? = null
+    while (hd != null) {
+        var next = hd.next
+        hd.next = first
+        first = hd
+        hd = next
     }
-    return newhead
+    return first
 }
 
 // recursive implementation
-private fun reverseList2(head: ListNode): ListNode? {
-    return reverse(head, null)
-}
-
-private fun reverse(head: ListNode?, newhead: ListNode?): ListNode? {
-    if (head == null) {
-        return newhead
+private fun reverseList2(head: ListNode?): ListNode? {
+    fun recur(curr: ListNode?, hd: ListNode?): ListNode? {
+        if (hd == null) return curr
+        var next = hd.next
+        hd.next = curr
+        return recur(hd, next)
     }
-    var next = head.next
-    head.next = newhead
-    return reverse(next, head)
-}
-
-fun reverseList4(head: ListNode?): ListNode? {
-    if (head == null || head.next == null) return head;
-
-    return reverseList4(head.next).apply {
-        head.next!!.next = head;
-        head.next = null;
-    }
+    return recur(null, head)
 }
