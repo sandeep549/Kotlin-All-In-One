e21518b677f57716d667b8d81592c65fa52e1693
Sandeep K <sandeepkumar.dev@gmail.com>
2020-01-14 10:14:08 +0530

    248. Strobogrammatic Number III.kt

---
+++ app/src/main/java/com/example/kotlinsamples/leetcode/kotlin/math/248. Strobogrammatic Number III.kt
@@ -0,0 +1,20 @@
+package com.example.kotlinsamples.leetcode.kotlin.math
+
+/**
+A strobogrammatic number is a number that looks the same when rotated 180 degrees (looked at upside down).
+
+Write a function to count the total strobogrammatic numbers that exist in the range of low <= num <= high.
+
+Example:
+
+Input: low = "50", high = "100"
+Output: 3
+Explanation: 69, 88, and 96 are three strobogrammatic numbers.
+Note:
+Because the range might be a large number, the low and high numbers are represented as string.
+ */
+
+
+fun strobogrammaticInRange(low: String, high: String): Int {
+    TODO()
+}
