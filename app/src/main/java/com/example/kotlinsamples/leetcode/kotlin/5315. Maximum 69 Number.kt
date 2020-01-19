c10ca4362a9fcdc2888615bad3a2a3fdc648968f
Sandeep K <sandeepkumar.dev@gmail.com>
2020-01-19 10:52:34 +0530

    5315. Maximum 69 Number.kt

---
+++ app/src/main/java/com/example/kotlinsamples/leetcode/kotlin/5315. Maximum 69 Number.kt
@@ -0,0 +1,17 @@
+package com.example.kotlinsamples.leetcode.kotlin
+
+private fun maximum69Number(num: Int): Int {
+    return num.toString().replaceFirst("6", "9").toInt()
+}
+
+// the interview way
+private fun maximum69Number2(num: Int): Int {
+    var arr = num.toString().toCharArray()
+    for (i in arr.indices) {
+        if (arr[i] == '6') {
+            arr[i] = '9'
+            break
+        }
+    }
+    return String(arr).toInt() // don't use arr.toString()
+}
