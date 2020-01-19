a03c8a03c21b12075abfda0ddec603b4a30e9ce6
Sandeep K <sandeepkumar.dev@gmail.com>
2020-01-19 12:23:25 +0530

    56. Merge Intervals.kt

--- app/src/main/java/com/example/kotlinsamples/leetcode/kotlin/array/56. Merge Intervals.kt
+++ app/src/main/java/com/example/kotlinsamples/leetcode/kotlin/array/56. Merge Intervals.kt
@@ -1,24 +1,19 @@
 package com.example.kotlinsamples.leetcode.kotlin.array
 
 private fun merge(intervals: Array<IntArray>): Array<IntArray> {
-    var input = intervals.toList()
-    if (input.isEmpty()) return emptyArray()
-
-    input = input.sortedWith(Comparator { t1, t2 -> t1[0] - t2[0] })
-
+    if (intervals.isEmpty()) return emptyArray()
+    var input = intervals.sortedWith(Comparator { t1, t2 -> t1[0] - t2[0] })
     var ans = ArrayList<IntArray>()
     var item = input[0]
     var curr: IntArray? = null
     for (i in 1..input.lastIndex) {
         curr = input[i]
-        if (curr[0] <= item[1]) {
-            item[1] = item[1].coerceAtLeast(curr[1])
-        } else {
+        if (curr[0] <= item[1]) item[1] = item[1].coerceAtLeast(curr[1])
+        else {
             ans.add(item)
             item = curr
         }
     }
     if (ans.isEmpty() || item[1] > ans.last()[1]) ans.add(item)
-
     return ans.toTypedArray()
 }
