a18c43f7e1f667f4950313e3c3c8d92e1a52f9c5
Sandeep K <sandeepkumar.dev@gmail.com>
2020-01-20 20:07:04 +0530

    98. Validate Binary Search Tree.kt

--- app/src/main/java/com/example/kotlinsamples/leetcode/kotlin/tree/94. Binary Tree Inorder Traversal.kt
+++ app/src/main/java/com/example/kotlinsamples/leetcode/kotlin/tree/94. Binary Tree Inorder Traversal.kt
@@ -3,6 +3,8 @@ package com.example.kotlinsamples.leetcode.kotlin.tree
 import java.util.*
 import kotlin.collections.ArrayList
 
+// TODO check morris traversal also
+
 private fun inorderTraversal(root: TreeNode?): List<Int> {
     val list = ArrayList<Int>()
     fun dfs(node: TreeNode?) {
@@ -18,7 +20,7 @@ private fun inorderTraversal(root: TreeNode?): List<Int> {
 private fun inorderTraversal2(root: TreeNode?): List<Int> {
     val list = ArrayList<Int>()
     var curr = root
-    val stack = ArrayDeque<TreeNode>()
+    val stack = ArrayDeque<TreeNode>() //Stack is legacy and deprecated
     while (curr != null || !stack.isEmpty()) {
         while (curr != null) {
             stack.push(curr)
@@ -27,6 +29,23 @@ private fun inorderTraversal2(root: TreeNode?): List<Int> {
         curr = stack.pop()
         list.add(curr.`val`)
         curr = curr.right
+    }
+    return list
+}
+
+private fun inorderTraversal3(root: TreeNode?): List<Int> {
+    val list = ArrayList<Int>()
+    var curr = root
+    val stack = ArrayDeque<TreeNode>() //Stack is legacy and deprecated
+    while (curr != null || !stack.isEmpty()) {
+        if (curr != null) {
+            stack.push(curr)
+            curr = curr.left
+        } else {
+            curr = stack.pop()
+            list.add(curr.`val`)
+            curr = curr.right
+        }
     }
     return list
 }
---
+++ app/src/main/java/com/example/kotlinsamples/leetcode/kotlin/tree/98. Validate Binary Search Tree.kt
@@ -0,0 +1,44 @@
+package com.example.kotlinsamples.leetcode.kotlin.tree
+
+import java.util.*
+
+private fun isValidBST(root: TreeNode?): Boolean {
+    fun dfs(node: TreeNode?, l: Int?, r: Int?): Boolean {
+        if (node == null) return true
+        if (l != null && node.`val` <= l) return false
+        if (r != null && node.`val` >= r) return false
+        return dfs(node.left, l, node.`val`) && dfs(node.right, node.`val`, r)
+    }
+    return dfs(root, null, null)
+}
+
+//same as above only kotlin construct difference
+private fun isValidBST2(root: TreeNode?): Boolean {
+    fun dfs(node: TreeNode?, l: Int?, r: Int?): Boolean {
+        node ?: return true
+        l?.let { if (node.`val` <= it) return false }
+        r?.let { if (node.`val` >= it) return false }
+        return dfs(node.left, l, node.`val`) && dfs(node.right, node.`val`, r)
+    }
+    return dfs(root, null, null)
+}
+
+private fun isValidBST3(root: TreeNode?): Boolean {
+    val stack: Deque<TreeNode> = ArrayDeque()
+    var inorder: Int? = null
+    var curr = root
+    while (!stack.isEmpty() || curr != null) {
+        while (curr != null) {
+            stack.push(root)
+            curr = curr.left
+        }
+        curr = stack.pop()
+        // If next element in inorder traversal
+        // is smaller than the previous one
+        // that's not BST.
+        if (inorder != null && curr.`val` <= inorder) return false
+        inorder = curr.`val`
+        curr = curr.right
+    }
+    return true
+}
