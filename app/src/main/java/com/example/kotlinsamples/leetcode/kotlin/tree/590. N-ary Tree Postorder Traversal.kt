85a67cb90f621af3a7249929a4e4c710fde8c4f8
Sandeep K <sandeepkumar.dev@gmail.com>
2020-01-23 13:24:11 +0530

    590. N-ary Tree Postorder Traversal.kt

---
+++ app/src/main/java/com/example/kotlinsamples/leetcode/kotlin/tree/590. N-ary Tree Postorder Traversal.kt
@@ -0,0 +1,51 @@
+package com.example.kotlinsamples.leetcode.kotlin.tree
+
+import java.util.*
+
+// Recursive
+private fun postorder(root: NTreeNode?): List<Int> {
+    var ans = mutableListOf<Int>()
+    fun process(node: NTreeNode?) = node?.let { ans.add(it.`val`) }
+
+    fun dfs(root: NTreeNode?) {
+        root?.let {
+            it.children?.let {
+                for (node in it) dfs(node)
+            }
+            process(it)
+        }
+    }
+    dfs(root)
+
+    return ans
+}
+
+// iterative, not verified on leetcode, no env. available to run for this question
+private fun postorder2(root: NTreeNode?): List<Int> {
+    val result = mutableListOf<Int>()
+    val stack = ArrayDeque<NTreeNode>()
+    if (root == null) {
+        return result
+    }
+
+    stack.push(root)
+    var cur = root
+    var pre: NTreeNode? = null
+    while (!stack.isEmpty()) {
+        cur = stack.peek()
+        if ((cur.children == null || cur.children!!.size == 0) || (pre != null && cur.children!!.contains(
+                pre
+            ))
+        ) {
+            result.add(cur.`val`)
+            stack.pop() //remove stack top as its processed
+            pre = cur //for backtracking
+        } else {
+            cur.children?.let {
+                for (node in it.reversed()) stack.push(node)
+            }
+        }
+    }
+
+    return result
+}
