9402cc24c41e604d7b0d368e03f76f9e89524403
Sandeep K <sandeepkumar.dev@gmail.com>
2020-01-24 20:56:06 +0530

    199. Binary Tree Right Side View.kt

---
+++ app/src/main/java/com/example/kotlinsamples/leetcode/kotlin/tree/199. Binary Tree Right Side View.kt
@@ -0,0 +1,55 @@
+package com.example.kotlinsamples.leetcode.kotlin.tree
+
+import java.util.*
+import kotlin.collections.ArrayList
+
+
+private fun rightSideView(root: TreeNode?): List<Int> {
+    var list = mutableListOf<Pair<Int, Int>>()
+    var set = mutableSetOf<Int>()
+    fun dfs(root: TreeNode?, level: Int) {
+        root?.let {
+            dfs(root.right, level + 1)
+            if (!set.contains(level)) {
+                list.add(Pair(level, root.`val`))
+                set.add(level)
+            }
+            dfs(root.left, level + 1)
+        }
+
+    }
+    dfs(root, 0)
+    list.sortBy { it.first }
+    return list.map { it.second }
+}
+
+private fun rightSideView2(root: TreeNode?): List<Int> {
+    var list = mutableListOf<Int>()
+    fun dfs(root: TreeNode?, level: Int) {
+        root?.let {
+            if (list.size < level) list.add(root.`val`)
+            dfs(root.right, level + 1)
+            dfs(root.left, level + 1)
+        }
+
+    }
+    dfs(root, 1)
+    return list
+}
+
+fun rightSideView3(root: TreeNode?): List<Int?>? { // reverse level traversal
+    val result: MutableList<Int?> = ArrayList()
+    val queue: Queue<TreeNode> = LinkedList()
+    if (root == null) return result
+    queue.offer(root)
+    while (!queue.isEmpty()) {
+        val size = queue.size
+        for (i in 0 until size) {
+            val cur: TreeNode = queue.poll()
+            if (i == 0) result.add(cur.`val`)
+            if (cur.right != null) queue.offer(cur.right)
+            if (cur.left != null) queue.offer(cur.left)
+        }
+    }
+    return result
+}
--- app/src/main/java/com/example/kotlinsamples/leetcode/kotlin/z_ImportantCodeSnippet/list_map_set_stack_queue.kt
+++ app/src/main/java/com/example/kotlinsamples/leetcode/kotlin/z_ImportantCodeSnippet/list_map_set_stack_queue.kt
@@ -35,8 +35,46 @@ private fun mapsInKotlin() {
     var likedhashmap = linkedMapOf<Int, Int>() // java LinkedHashMap, mutable, same as 2 internally
 }
 
+fun main() {
+    PleaseRunToBetterUnderstandMe()
+}
+
+private fun PleaseRunToBetterUnderstandMe() {
+    val m: Deque<Int> = LinkedList()//ArrayDeque()
+    m.push(1)
+    m.push(2)
+    m.push(3)
+    println(m.toList())
+    println(m.peek())
+    println(m.peekFirst())
+    println(m.peekLast())
+    m.offer(4)
+    println(m.toList())
+    println(m.peek())
+    println(m.peekFirst())
+    println(m.peekLast())
+    m.add(5)
+    println(m.toList())
+    m.addFirst(6)
+    m.addLast(7)
+    println(m.toList())
+    m.pop()
+    println(m.toList())
+    m.push(8)
+    println(m.toList())
+    m.poll()
+    println(m.toList())
+}
+
+/**
+ * Stack:
+ * push()
+ * pop()
+ * peek()
+ */
 private fun stackInKotlin() {
-    val stack1: ArrayDeque<Int> = ArrayDeque()
+    //1
+    val stack1: Deque<Int> = ArrayDeque()
     stack1.push(1)
     stack1.push(2)
     stack1.pop()
@@ -44,14 +82,43 @@ private fun stackInKotlin() {
     stack1.peekFirst()
     stack1.isEmpty()
     stack1.size
+
+    //2
+    val stack2: Deque<Int> = LinkedList()
+    stack2.push(1)
+    stack2.push(2)
+    stack2.pop()
+    stack2.peek()
+    stack2.peekFirst()
+    stack2.isEmpty()
+    stack2.size
+
 }
 
+
+/**
+ * Queue:
+ * offer(), add()
+ * remove(), poll()
+ * peek()
+ */
 private fun queueInKotlin() {
-    val queue1: ArrayDeque<Int> = ArrayDeque()
+    //1
+    val queue1: Deque<Int> = ArrayDeque()
     queue1.add(1)
     queue1.add(2)
     queue1.remove()
     queue1.peekFirst()
     queue1.isEmpty()
     queue1.size
+
+    //2
+    var queue2: Deque<Int> = LinkedList()
+    queue2.offer(1)
+    queue2.offer(2)
+    queue2.offer(3)
+    println(queue2.toList())
+    queue2.poll()
+    println(queue2.toList())
+    println(queue2.peek())
 }
