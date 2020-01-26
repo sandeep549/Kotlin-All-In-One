3c0fe923c8d4244a2e024ea6e88c802f7fe06daa
Sandeep K <sandeepkumar.dev@gmail.com>
2020-01-26 19:53:06 +0530

    5320. Filter Restaurants by Vegan-Friendly, Price and Distance.kt

---
+++ app/src/main/java/com/example/kotlinsamples/leetcode/kotlin/misc/5320. Filter Restaurants by Vegan-Friendly, Price and Distance.kt
@@ -0,0 +1,17 @@
+package com.example.kotlinsamples.leetcode.kotlin.misc
+
+private fun filterRestaurants(
+    restaurants: Array<IntArray>,
+    veganFriendly: Int,
+    maxPrice: Int,
+    maxDistance: Int
+): List<Int> {
+    return restaurants
+        .filter { it[2] >= veganFriendly } //Vegan-Friendly or Both
+        .filter { it[3] <= maxPrice }
+        .filter { it[4] <= maxDistance }
+        .sortedWith(
+            compareByDescending<IntArray> { it[1] } //sort by descending rating
+                .thenByDescending { it[0] }) //if rating match, then descending id
+        .map { it[0] } //gather only id's
+}
