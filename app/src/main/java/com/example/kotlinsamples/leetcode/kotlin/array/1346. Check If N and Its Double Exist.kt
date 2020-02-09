c44f5dfab0812c44cf14337417b57d8d353188d5
Sandeep K <sandeepkumar.dev@gmail.com>
2020-02-09 11:51:04 +0530

    1346. Check If N and Its Double Exist.kt

---
+++ app/src/main/java/com/example/kotlinsamples/leetcode/kotlin/array/1346. Check If N and Its Double Exist.kt
@@ -0,0 +1,10 @@
+package com.example.kotlinsamples.leetcode.kotlin.array
+
+private fun checkIfExist(arr: IntArray): Boolean {
+    var set = mutableSetOf<Int>()
+    for (a in arr) {
+        if ((a % 2 == 0 && set.contains(a / 2)) || set.contains(2 * a)) return true
+        set.add(a)
+    }
+    return false
+}
