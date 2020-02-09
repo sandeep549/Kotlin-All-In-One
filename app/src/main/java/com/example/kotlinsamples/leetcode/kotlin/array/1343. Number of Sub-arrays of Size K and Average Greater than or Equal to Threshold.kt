c06f92d021067a679defd331ffc9ba5465a79a1e
Sandeep K <sandeepkumar.dev@gmail.com>
2020-02-09 11:29:15 +0530

    1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.kt

---
+++ app/src/main/java/com/example/kotlinsamples/leetcode/kotlin/array/1343. Number of Sub-arrays of Size K and Average Greater than or Equal to Threshold.kt
@@ -0,0 +1,14 @@
+package com.example.kotlinsamples.leetcode.kotlin.array
+
+private fun numOfSubarrays(arr: IntArray, k: Int, threshold: Int): Int {
+    var count = 0
+    var sum = 0
+    var i = 0
+    while (i < k - 1) sum += arr[i++]
+    for (j in i..arr.lastIndex) {
+        sum += arr[j]
+        if (sum / k >= threshold) count++
+        sum -= arr[j - k + 1]
+    }
+    return count
+}
