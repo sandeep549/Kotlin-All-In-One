f03118f4d268525528a09d316d8c74e5f01a67b2
Sandeep K <sandeepkumar.dev@gmail.com>
2020-02-13 12:38:06 +0530

    287. Find the Duplicate Number.kt

---
+++ app/src/main/java/com/example/kotlinsamples/leetcode/kotlin/array/medium/287. Find the Duplicate Number.kt
@@ -0,0 +1,28 @@
+package com.example.kotlinsamples.leetcode.kotlin.array.medium
+
+private fun findDuplicate(nums: IntArray): Int {
+    var set = mutableSetOf<Int>()
+    for (a in nums) {
+        if (set.contains(a)) return a
+        set.add(a)
+    }
+    return 0 //we must never come here
+}
+
+//O(n); floyd cycle detection
+private fun findDuplicate2(nums: IntArray): Int {
+    var slow = nums[0]
+    var fast = nums[0]
+    do {
+        slow = nums[slow]
+        fast = nums[nums[fast]]
+    } while (slow != fast)
+
+    // Find the "entrance" to the cycle.
+    fast = nums[0]
+    while (fast != slow) {
+        fast = nums[fast]
+        slow = nums[slow]
+    }
+    return slow
+}
