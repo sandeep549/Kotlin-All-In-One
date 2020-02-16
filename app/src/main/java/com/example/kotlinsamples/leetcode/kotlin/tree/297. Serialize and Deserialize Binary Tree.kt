9c1d979f7dd5b36b1decbb2ed90f6a5a99d44ae0
Sandeep K <sandeepkumar.dev@gmail.com>
2020-02-16 19:10:54 +0530

    297. Serialize and Deserialize Binary Tree.kt

--- app/src/main/java/com/example/kotlinsamples/leetcode/kotlin/tree/297. Serialize and Deserialize Binary Tree.kt
+++ app/src/main/java/com/example/kotlinsamples/leetcode/kotlin/tree/297. Serialize and Deserialize Binary Tree.kt
@@ -1,38 +1,46 @@
 package com.example.kotlinsamples.leetcode.kotlin.tree
 
 import java.util.*
+import java.util.Arrays.asList
 
 
 private class Codec {
+    private val spliter = ","
+    private val NN = "X"
+
     // Encodes a tree to a single string.
     fun serialize(root: TreeNode?): String? {
-        var str = StringBuilder()
-        fun serialise(root: TreeNode?, str: StringBuilder): StringBuilder {
-            if (root == null) str.append("N").append(",")
-            str.append(root!!.`val`).append(",")
-            serialise(root.left, str)
-            serialise(root.right, str)
-            return str
+        val sb = StringBuilder()
+        buildString(root, sb)
+        return sb.toString()
+    }
+
+    private fun buildString(node: TreeNode?, sb: StringBuilder) {
+        if (node == null) {
+            sb.append(NN).append(spliter)
+        } else {
+            sb.append(node.`val`).append(spliter)
+            buildString(node.left, sb)
+            buildString(node.right, sb)
         }
-        serialise(root, str)
-        return str.toString()
     }
 
     // Decodes your encoded data to tree.
-    fun deserialize(data: String?): TreeNode? {
-        val nodes: Deque<String> = LinkedList()
-        nodes.addAll(data!!.split(","))
-        fun deserialise(nodes: Deque<String>): TreeNode? {
-            var value = nodes.remove()
-            if (value == "X") {
-                return null
-            } else {
-                var node = TreeNode(value.toInt())
-                node.left = deserialise(nodes)
-                node.right = deserialise(nodes)
-                return node
-            }
+    fun deserialize(data: String): TreeNode? {
+        val nodes = ArrayDeque<String>()
+        nodes.addAll(data.split(spliter))
+        return buildTree(nodes)
+    }
+
+    private fun buildTree(nodes: Deque<String>): TreeNode? {
+        val `val` = nodes.remove()
+        if (`val` == NN) {
+            return null
+        } else {
+            val node = TreeNode(Integer.valueOf(`val`))
+            node.left = buildTree(nodes)
+            node.right = buildTree(nodes)
+            return node
         }
-        return deserialise(nodes)
     }
 }
