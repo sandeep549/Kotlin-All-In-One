4e9af06ed43906fa8c9e126983a2990e747f5306
Sandeep K <sandeepkumar.dev@gmail.com>
2020-03-10 14:04:55 +0530

    Apply kotlin DSL for build gradle files

---
+++ app/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("android.extensions")
+}
+
+android {
+    compileSdkVersion(28)
+    defaultConfig {
+        applicationId = "com.example.kotlinsamples"
+        minSdkVersion(21)
+        targetSdkVersion(28)
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.50")
+    implementation("com.android.support:appcompat-v7:28.0.0")
+    implementation("com.android.support.constraint:constraint-layout:1.1.3")
+
+    implementation("com.google.code.gson:gson:2.8.5")
+    implementation("org.danilopianini:gson-extras:0.2.1")
+    
+    testImplementation("junit:junit:4.12")
+    androidTestImplementation("com.android.support.test:runner:1.0.2")
+    androidTestImplementation("com.android.support.test.espresso:espresso-core:3.0.2")
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,21 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
+plugins {
+    id("org.jlleitschuh.gradle.ktlint") version "7.1.0"
+}
+
 buildscript {
-    ext.kotlin_version = '1.3.21'
+    val kotlinVersion = "1.3.50"
     repositories {
         google()
         jcenter()
-        
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.5.3'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+        classpath("com.android.tools.build:gradle:3.5.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
-}
-
-plugins {
-    id "org.jlleitschuh.gradle.ktlint" version "7.1.0"
 }
 
 allprojects {
@@ -24,9 +23,10 @@ allprojects {
         google()
         jcenter()
     }
-    apply plugin: "org.jlleitschuh.gradle.ktlint"
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks {
+    val clean by registering(Delete::class) {
+        delete(buildDir)
+    }
 }
