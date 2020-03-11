a6df304a3e67fbc461ae42baabe031a0bcc0f9d5
Sandeep K <sandeepkumar.dev@gmail.com>
2020-03-11 12:49:32 +0530

    Apply 'Ktlint Gradle' plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
-    id("org.jlleitschuh.gradle.ktlint") version "7.1.0"
+    id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
 }
 
 buildscript {
@@ -14,6 +14,7 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
+        classpath("org.jlleitschuh.gradle:ktlint-gradle:9.2.1")
     }
 }
 
@@ -21,6 +22,15 @@ allprojects {
     repositories {
         google()
         jcenter()
+    }
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint") // Version should be inherited from parent
+
+    // Optionally configure plugin
+    ktlint {
+        debug.set(true)
     }
 }
 
